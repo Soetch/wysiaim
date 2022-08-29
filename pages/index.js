63b528f7ae4fd7aa13c4c461f89c8ff4913c518e
Marceau Malegeant <soetchdev@gmail.com>
2022-08-29 16:35:02 +0200

    2

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,17 @@ import Ad from "../components/Ad"
 import Link from "next/link"
 import Header from "../components/Header"
 import Members from "../components/Members"
+import Head from "next/head"
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <meta charset="UTF-8"/>
+        <meta name="description" content="WYSIAim's Home"/>
+        <meta name="keywords" content="WYSIAim, osu!, taiko, mania, catch"/>
+        <meta name="author" content="WYSIAim"/>
+      </Head>
       <div className="">
         <Header/>
         <div id="body">
